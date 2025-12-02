704d22add0d38926d35578ef8a733b3cc3f442b0
Shems <104856722+shems95v@users.noreply.github.com>
2025-12-02 15:23:48 +0100

    Update histo.h

--- histo.h
+++ histo.h
@@ -1,14 +1,14 @@
 #ifndef HISTO_H
 #define HISTO_H
 
-typedefstruct {
-  
-  char id[50];
-double capacite_max;
-double volume_source;
-double volume_reel;
+#include <stdio.h>
 
-}Usine;
+typedef struct {
+    char id[50];
+    double capacite_max;
+    double volume_source;
+    double volume_reel;
+} Usine;
 
 void afficher_usine (Usine u);
 #endif
