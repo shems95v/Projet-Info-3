e76e655314425071bd588ca9233350a112893b61
Taissir03 <taissirrguig@gmail.com>
2025-12-02 15:19:56 +0100

    Update histo.h

--- histo.h
+++ histo.h
@@ -1,1 +1,14 @@
+#ifndef HISTO_H
+#define HISTO_H
 
+typedefstruct {
+  
+  char id[50];
+double capacite_max;
+double volume_source;
+double volume_reel;
+
+}Usine;
+
+void afficher_usine (Usine u);
+#endif
