e76937c3a064f42f9b8205ae4b7dc45ed75f4bf7
Sara158209 <sara.belghazi@etu.cyu.fr>
2025-12-12 10:15:23 +0100

    script.sh
    Add argument checks and file existence verification

--- script.sh
+++ script.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 
+START=$(date +%s)
+
+if [$# -lt 2 ]; then
+echo "Erreur : Pas assez d'arguments"
+exit (1)
+fi
+ 
+arg1="$1"   (nom fichier CSV/DATA) 
+arg2="$2"
+arg3="$3"
+
+ #Verification de lexistence du fichier
+ 
+ if [! -f "$arg1" ] then
+ echo " Fichier $arg1 introuvable "
+ exit (1)
+ fi
+ 
+
+ 
+
