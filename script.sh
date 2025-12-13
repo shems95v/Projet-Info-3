deafd479e0f7831c37322d719f7b4a4a152cd1a6
Sara158209 <sara.belghazi@etu.cyu.fr>
2025-12-13 20:47:33 +0100

    Fix argument validation and error handling in script
    Corrected argument checks and improved error messages in script.sh.

--- script.sh
+++ script.sh
@@ -2,22 +2,54 @@
 
 START=$(date +%s)
 
-if [$# -lt 2 ]; then
-echo "Erreur : Pas assez d'arguments"
-exit (1)
+if [$# -ne 3 ]; then
+echo "Erreur : Pas assez darguments"
+exit 1
 fi
- 
-arg1="$1"   (nom fichier CSV/DATA) 
-arg2="$2"
-arg3="$3"
+
+DATAFILE="$1"  # chemin du fichier de donnees 
+CHOIX="$2"     # histo ou leaks
+OPTION="$3"    # max/src/real ou id usine
+
 
  #Verification de lexistence du fichier
  
- if [! -f "$arg1" ] then
- echo " Fichier $arg1 introuvable "
- exit (1)
+ if [! -f "$DATAFILE" ] then
+ echo " Erreur : fichier '$DATAFILE' introuvable "
+ exit 1
  fi
- 
 
+#Verification des separateurs points virgule
+
+ if ! grep -q ";" "$DATAFILE"; then
+ echo "Erreur : fichier invalide (pas de separateur ';')"
+ exit 1
+ fi
+
+#Verification des commandes
  
+if [ "$CHOIX" = "histo" ]; then
+if [ "$OPTION" != "max" ] && [ "$OPTION" != "real" ] && [ "$OPTION" != "src" ]; then
+echo "Erreur : option histo incorrecte"
+exit 1
+fi
+
+elif [ "$CHOIX" = "leaks" ]; then
+if [ -z "$OPTION" ]; then
+echo "Erreur : id dusine manquant"
+exit 1
+fi
+
+else
+echo "Erreur : commande inconnue"
+exit 1
+fi
+
+
+
+
+
+END=$(date +%s)
+DURATION=$(( (END - START) * 1000 ))
+echo "Durée totale : ${DURATION} ms"
 
